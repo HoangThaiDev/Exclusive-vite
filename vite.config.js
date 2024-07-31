7db52cb1e78148667578a18228c0ca3a24c0c2c6
HoangThaiDev <thaindev2000@gmail.com>
2024-07-31 18:00:13 +0700

    Fix file package.json

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === "production" ? "/Exclusive-web/" : "/",
+    base: mode === "production" ? "/Exclusive-vite/" : "/",
     server: {
       port: 3000,
     },
